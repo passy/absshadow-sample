de8039f541c7880246cd4ec39bcf4e15b3551fb7
Pascal Hartig <phartig@rdrei.net>
2012-02-23 16:00:11 +0100

    Access action bar

--- src/main/java/net/rdrei/android/absshadow/MainActivity.java
+++ src/main/java/net/rdrei/android/absshadow/MainActivity.java
@@ -1,9 +1,10 @@
 package net.rdrei.android.absshadow;
 
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import com.actionbarsherlock.app.ActionBar;
 import android.os.Bundle;
 
-public class MainActivity extends Activity
+public class MainActivity extends FragmentActivity
 {
     /** Called when the activity is first created. */
     @Override
@@ -11,5 +12,8 @@ public class MainActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        ActionBar actionBar = this.getSupportActionBar();
+        actionBar.setDisplayShowTitleEnabled(true);
     }
 }
