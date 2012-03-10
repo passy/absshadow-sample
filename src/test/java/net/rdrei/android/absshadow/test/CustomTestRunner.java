d83b7b289ac9432b3616717deace7691a3df9134
Pascal Hartig <phartig@rdrei.net>
2012-03-10 03:10:15 +0100

    Shadow SherlockActivity

--- src/test/java/net/rdrei/android/absshadow/test/CustomTestRunner.java
+++ src/test/java/net/rdrei/android/absshadow/test/CustomTestRunner.java
@@ -1,5 +1,7 @@
 package net.rdrei.android.absshadow.test;
 
+import net.rdrei.android.absshadow.test.shadow.ShadowSherlockActivity;
+
 import org.junit.runners.model.InitializationError;
 
 import com.xtremelabs.robolectric.Robolectric;
@@ -9,10 +11,12 @@ public class CustomTestRunner extends RobolectricTestRunner {
 
     public CustomTestRunner(Class<?> testClass) throws InitializationError {
         super(testClass);
+		addClassOrPackageToInstrument("com.actionbarsherlock.app.SherlockActivity");
     }
 
     @Override
     protected void bindShadowClasses() {
         super.bindShadowClasses();
+		Robolectric.bindShadowClass(ShadowSherlockActivity.class);
     }
 }
